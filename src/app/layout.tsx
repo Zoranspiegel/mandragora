8d838962105cc19329e64630e37187568ae11706
Zoranspiegel <zoranbow@gmail.com>
2026-07-27 12:23:13 -0500

    responsive root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,9 @@ export default function RootLayout({
         "font-sans",
       )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col sm:w-md sm:min-h-[90dvh] sm:my-[5dvh] sm:m-auto sm:border-foreground sm:border-4 sm:border-double sm:rounded-2xl sm:shadow-lg">
+        {children}
+      </body>
     </html>
   );
 }
