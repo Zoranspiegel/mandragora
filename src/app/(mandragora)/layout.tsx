6b8f7e37f9f7917132a3c5ebe46223d55974345b
Zoranspiegel <zoranbow@gmail.com>
2026-07-28 14:04:40 -0500

    auth & mandragora next groups with layouts

---
+++ src/app/(mandragora)/layout.tsx
@@ -0,0 +1,12 @@
+// import { getServerSession } from "@/lib/get-server-session";
+// import { redirect } from "next/navigation";
+
+export default async function MandragoraLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  // const session = await getServerSession();
+
+  // if (!session) redirect("/login");
+
+  return <div>{children}</div>;
+}
