1d6b38f6d0f11385cbac77f348b3d93b514e5301
Zoranspiegel <zoranbow@gmail.com>
2026-08-28 23:46:03 -0500

    ModalProvider rearranged

--- src/app/(mandragora)/calendar/[date]/calendar.tsx
+++ src/app/(mandragora)/calendar/[date]/calendar.tsx
@@ -2,7 +2,6 @@
 
 import { useCalendar } from "@/hooks/swr/useCalendar";
 import CalendarTile from "./calendar-tile";
-import { ModalContextProvider } from "@/contexts/modal-context";
 
 export default function Calendar({ date }: { date: string }) {
   const { calendar, error, isLoading } = useCalendar(date);
@@ -30,9 +29,7 @@ export default function Calendar({ date }: { date: string }) {
           {calendar.calendar.map((row, i) => (
             <div key={i} className="flex justify-between gap-3">
               {row.map((tile, j) => (
-                <ModalContextProvider key={j + tile.day}>
-                  <CalendarTile tile={tile} date={date} />
-                </ModalContextProvider>
+                <CalendarTile key={j + tile.day} tile={tile} date={date} />
               ))}
             </div>
           ))}
--- src/app/(mandragora)/layout.tsx
+++ src/app/(mandragora)/layout.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "@/lib/get-server-session";
 import { redirect } from "next/navigation";
 import MandragoraNavbar from "./mandragora-navbar";
 import MandragoraMenubar from "./mandragora-menubar";
+import { ModalContextProvider } from "@/contexts/modal-context";
 
 export default async function MandragoraLayout({
   children,
@@ -11,12 +12,14 @@ export default async function MandragoraLayout({
   if (!session) redirect("/login");
 
   return (
-    <div className="h-full grid grid-rows-[75px_1fr_75px]">
-      <MandragoraMenubar />
-      <main className="h-full relative flex flex-col p-lg pt-0 overflow-y-scroll scrollbar-none">
-        {children}
-      </main>
-      <MandragoraNavbar />
-    </div>
+    <ModalContextProvider>
+      <div className="h-full grid grid-rows-[75px_1fr_75px]">
+        <MandragoraMenubar />
+        <main className="h-full relative flex flex-col p-lg pt-0 overflow-y-scroll scrollbar-none">
+          {children}
+        </main>
+        <MandragoraNavbar />
+      </div>
+    </ModalContextProvider>
   );
 }
